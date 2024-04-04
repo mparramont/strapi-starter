13c39b024b2ac30ec2b4aaee1fc22506705076fd
Miguel Parramón Teixidó <mparramon@palo-it.com>
2024-04-04 18:02:34 +0800

    ci: more stuff

--- config/database.ts
+++ config/database.ts
@@ -58,9 +58,9 @@ export default ({ env }) => {
         connectionString: env("DATABASE_URL"),
         host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        database: env("DATABASE_NAME", "strapi-starter-database"),
+        user: env("DATABASE_USERNAME", "admin"),
+        password: env("DATABASE_PASSWORD", "admin"),
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
--- config/env/test/database.ts
+++ config/env/test/database.ts
@@ -1,29 +1,38 @@
-export default ({ env }) => ({
-  connection: {
-    client: env("TEST_DATABASE_CLIENT", "postgres"),
+export default ({ env }) => {
+  console.log({
+    host: env("TEST_DATABASE_HOST", "localhost"),
+    port: env.int("TEST_DATABASE_PORT", 5443),
+    database: env("TEST_DATABASE_NAME", "strapi-starter-test-database"),
+    user: env("TEST_DATABASE_USERNAME", "admin"),
+    password: env("TEST_DATABASE_PASSWORD", "admin"),
+  });
+  return {
     connection: {
-      connectionString: env("TEST_DATABASE_URL"),
-      host: env("TEST_DATABASE_HOST", "localhost"),
-      port: env.int("TEST_DATABASE_PORT", 5443),
-      database: env("TEST_DATABASE_NAME", "strapi-starter-test-database"),
-      user: env("TEST_DATABASE_USERNAME", "admin"),
-      password: env("TEST_DATABASE_PASSWORD", "admin"),
-      ssl: env.bool("TEST_DATABASE_SSL", false) && {
-        key: env("TEST_DATABASE_SSL_KEY", undefined),
-        cert: env("TEST_DATABASE_SSL_CERT", undefined),
-        ca: env("TEST_DATABASE_SSL_CA", undefined),
-        capath: env("TEST_DATABASE_SSL_CAPATH", undefined),
-        cipher: env("TEST_DATABASE_SSL_CIPHER", undefined),
-        rejectUnauthorized: env.bool(
-          "TEST_DATABASE_SSL_REJECT_UNAUTHORIZED",
-          true,
-        ),
+      client: env("TEST_DATABASE_CLIENT", "postgres"),
+      connection: {
+        connectionString: env("TEST_DATABASE_URL"),
+        host: env("TEST_DATABASE_HOST", "localhost"),
+        port: env.int("TEST_DATABASE_PORT", 5443),
+        database: env("TEST_DATABASE_NAME", "strapi-starter-test-database"),
+        user: env("TEST_DATABASE_USERNAME", "admin"),
+        password: env("TEST_DATABASE_PASSWORD", "admin"),
+        ssl: env.bool("TEST_DATABASE_SSL", false) && {
+          key: env("TEST_DATABASE_SSL_KEY", undefined),
+          cert: env("TEST_DATABASE_SSL_CERT", undefined),
+          ca: env("TEST_DATABASE_SSL_CA", undefined),
+          capath: env("TEST_DATABASE_SSL_CAPATH", undefined),
+          cipher: env("TEST_DATABASE_SSL_CIPHER", undefined),
+          rejectUnauthorized: env.bool(
+            "TEST_DATABASE_SSL_REJECT_UNAUTHORIZED",
+            true,
+          ),
+        },
+        schema: env("TEST_DATABASE_SCHEMA", "public"),
       },
-      schema: env("TEST_DATABASE_SCHEMA", "public"),
+      pool: {
+        min: env.int("TEST_DATABASE_POOL_MIN", 2),
+        max: env.int("TEST_DATABASE_POOL_MAX", 10),
+      },
     },
-    pool: {
-      min: env.int("TEST_DATABASE_POOL_MIN", 2),
-      max: env.int("TEST_DATABASE_POOL_MAX", 10),
-    },
-  },
-});
+  };
+};
